e7db2a2957f04701b44718c88569d2f79eb188c2
Felix Sarsah <62659177+Enimbuild@users.noreply.github.com>
2023-01-11 17:47:35 +0100

    Update predict_your_weather_App.py

--- predict_your_weather_App.py
+++ predict_your_weather_App.py
@@ -4,5 +4,5 @@ from pprint import pprint
 API_Key ='YOUR OWN API KEY'
 city= input("Enter a city: ")
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
-weather_data = requests.get(base_url).json()
-pprint(weather_data)
+weather_data = requests.get(base_url)
+pprint(weather_data.json())
